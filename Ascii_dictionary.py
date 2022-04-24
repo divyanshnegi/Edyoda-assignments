8fe2165f4ea3b79625367f797101e1619b1406ec
divyanshnegi <40057630+divyanshnegi@users.noreply.github.com>
2022-04-24 21:55:35 +0530

    Add files via upload

---
+++ Ascii_dictionary.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+ascii_dict={}#dictionary created 
+for i in range(ord("a"),ord("z")+1):#forloop
+    ascii_dict[chr(i)]=i
+     
+print(ascii_dict)#print result
+
+
+# In[ ]:
+
+
+
+
