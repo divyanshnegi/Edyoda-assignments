132a18445e1170a8f77afc875efc0c03f677fd23
divyanshnegi <40057630+divyanshnegi@users.noreply.github.com>
2022-05-01 21:56:24 +0530

    Add files via upload

---
+++ Summation of list .py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+list=[8,2,3,0,7]#list
+
+def Summation(x):#function
+     x=sum(x)
+     return x
+
+a=Summation(list)
+print("Summation of numbers in the list is:",a)
+
+
+# In[ ]:
+
+
+
+
